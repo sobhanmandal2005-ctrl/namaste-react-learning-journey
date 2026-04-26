e67e96c6ce4f3780ea6753d09a57b8c917a1ef28
sobhanmandal2005-ctrl <sobhanmandal2005@gmail.com>
2026-04-26 23:41:32 +0530

    episode - 02 Igniting the app

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
-const heading = React.createElement
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-("h1", {}, "Hello World from React🚀");
+
+
+
+const messege = "Namaste React🚀";
+
+const parent = React.createElement("div", {id: "parent"},[
+   React.createElement("div", {id: "child1"}, [
+    React.createElement("h1", {id: "heading_1"}, messege),
+    React.createElement("h2", {id: "heading_2"}, "this is h2 tag")
+  ]),
+   React.createElement("div", {id: "child2"}, [
+    React.createElement("h1", {id: "heading_1"}, "this is h1 tag"),
+    React.createElement("h2", {id: "heading_2"}, "this is h2 tag")
+  ]),
+   React.createElement("div", {id: "child3"}, [
+    React.createElement("p", {id: "para1"}, "My name is sobhan"),
+    React.createElement("button", {className: "button1"}, "Bomb")
+]),
+
+]);
+
+//const heading = React.createElement
+
+//("h1", {}, "Hello World from React🚀");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
