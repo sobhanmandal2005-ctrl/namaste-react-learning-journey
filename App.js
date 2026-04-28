c7b9d2361af339274e8145a23c54e0d9945614fd
sobhanmandal2005-ctrl <sobhanmandal2005@gmail.com>
2026-04-28 23:59:26 +0530

    ep-4 progress

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
+const Header = () => {
+  return (
+    <div id="header">
+      <div className="logo-container">
+        <img className="res-logo" src="magnific.com/free-vector/gradient-restaurant-logo-design_143804994.htm#fromView=keyword&page=1&position=5&uuid=5c813bbf-359e-4b0f-84db-05ce2fa8f414&query=Restaurant+logo"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>About us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const Heading = () => (<h1>Hyy this is me Sobhan</h1>);
-
-const title = (<h1>Here I am showing my react learining journey</h1>);
-
-const FirstComponent = () => (
-  <div id="container">
-    <Heading/>
-    {title}
-    <h1>Namaste react learning Journey🚀</h1>
-  </div>
-);
+const Body = () => {
+  return (
+    <div></div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div id="container">
+     <Header/>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<FirstComponent/>);
+root.render(<AppLayout/>);
